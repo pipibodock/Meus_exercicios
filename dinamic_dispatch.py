040eecd4733a5ac1a3bd672d8c25cab1bb31ab94
Luiz Fellipe <fellipe.mirandamatos@gmail.com>
2019-01-26 09:15:34 -0200

    exemplo dinamic dispatch

---
+++ dinamic_dispatch.py
@@ -0,0 +1,10 @@
+class Quadrado():
+    pass
+
+class Circulo():
+    pass
+
+instanciador = {"quadrado": Quadrado, "circulo": Circulo}
+
+circulo = intanciador["circulo"]()
+intanciador["quadrado"]()
